895b68469e20bb885dc294e479c16a164ab57651
Funmilayo A. Fagoyinbo <funmilayofagoyinbo@gmail.com>
2023-07-04 19:53:19 -0700

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Demonstrating my hands-on tutorial on ACG for GitOps"
+    return "Hello World! Demonstrating gitops"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
